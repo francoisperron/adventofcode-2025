84b7ea7cf76d38b45539dce16d761bdd9e43725b
Francois Perron <fperron@gmail.com>
2025-12-03 17:42:17 -0500

    Day 03 part 2

--- src/day03/banks.rs
+++ src/day03/banks.rs
@@ -26,11 +26,44 @@ impl From<&str> for Bank {
 }
 
 impl Bank {
-    pub fn max(&self) -> usize {
-        let largest = *self.batteries[..self.batteries.len() - 1].iter().max().unwrap();
-        let index = self.batteries.iter().position(|&b| b == largest).unwrap();
-        let second_largest = *self.batteries[index + 1..].iter().max().unwrap();
+    pub fn max(&self, batteries_count: usize) -> usize {
+        let mut nb = batteries_count;
+        let mut batteries = self.batteries.clone();
+        let mut joltage: usize = 0;
 
-        (largest * 10 + second_largest) as usize
+        while nb > 0 {
+            let largest = *batteries[..batteries.len() - nb + 1].iter().max().unwrap();
+            let largest_index = batteries.iter().position(|&b| b == largest).unwrap();
+
+            batteries = batteries[largest_index + 1..].to_vec();
+
+            joltage = (joltage * 10) + largest as usize;
+            nb -= 1;
+        }
+
+        joltage
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn max_2() {
+        let bank = Bank::from("818181911112111");
+        assert_eq!(bank.max(2), 92);
+    }
+
+    #[test]
+    fn max_3() {
+        let bank = Bank::from("818181911112111");
+        assert_eq!(bank.max(3), 921);
+    }
+
+    #[test]
+    fn max_12() {
+        let bank = Bank::from("818181911112111");
+        assert_eq!(bank.max(12), 888911112111);
     }
 }
--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -4,12 +4,17 @@ mod banks;
 
 fn part1(input: &str) -> usize {
     let banks = Banks::from(input);
-    banks.iter().map(|bank| bank.max()).sum()
+    banks.iter().map(|bank| bank.max(2)).sum()
+}
+
+fn part2(input: &str) -> usize {
+    let banks = Banks::from(input);
+    banks.iter().map(|bank| bank.max(12)).sum()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day03::part1;
+    use crate::day03::{part1, part2};
     use crate::toolbox::daily::daily_input;
 
     #[test]
@@ -21,6 +26,17 @@ mod tests {
     fn solves_part1() {
         let input = daily_input(3);
         assert_eq!(part1(&input), 17087);
+    }
+
+    #[test]
+    fn solves_part2_example() {
+        assert_eq!(part2(EXAMPLE), 3121910778619);
+    }
+
+    #[test]
+    fn solves_part2() {
+        let input = daily_input(3);
+        assert_eq!(part2(&input), 169019504359949);
     }
 
     const EXAMPLE: &str = "987654321111111\n811111111111119\n234234234234278\n818181911112111";
