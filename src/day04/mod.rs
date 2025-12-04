9b3f57911e31f8c6d41df3d9c117f5f56b10a602
Francois Perron <fperron@gmail.com>
2025-12-04 07:21:54 -0500

    Day 04 part 2

--- src/day04/mod.rs
+++ src/day04/mod.rs
@@ -8,10 +8,32 @@ fn part1(input: &str) -> usize {
         .count()
 }
 
+fn part2(input: &str) -> usize {
+    let mut grid = Grid::from(input);
+    let mut rolls_count = 0;
+
+    loop {
+        let rolls: Vec<_> = grid
+            .elements
+            .iter()
+            .filter(|&(&p, &v)| v == '@' && p.around_me().filter(|&p2| grid.element_at(&p2).eq(&'@')).count() < 4)
+            .map(|(&p, _)| p)
+            .collect();
+
+        if rolls.is_empty() {
+            break;
+        }
+
+        rolls.iter().for_each(|&p| grid.set_element_at(&p, '.'));
+        rolls_count += rolls.len();
+    }
+
+    rolls_count
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::day04::part1;
+    use crate::day04::{part1, part2};
     use crate::toolbox::daily::daily_input;
 
     #[test]
@@ -23,6 +45,17 @@ mod tests {
     fn solves_part1() {
         let input = daily_input(4);
         assert_eq!(part1(&input), 1457)
+    }
+
+    #[test]
+    fn solves_part2_example() {
+        assert_eq!(part2(EXAMPLE), 43)
+    }
+
+    #[test]
+    fn solves_part2() {
+        let input = daily_input(4);
+        assert_eq!(part2(&input), 8310)
     }
 
     const EXAMPLE: &str = "..@@.@@@@.
