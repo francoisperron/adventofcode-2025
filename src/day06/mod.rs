eae770325144eb9f6e412d23df053116ccad4c7d
Francois Perron <fperron@gmail.com>
2025-12-06 11:42:53 -0500

    Day 06 part 2

--- src/day06/mod.rs
+++ src/day06/mod.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 fn part1(input: &str) -> usize {
     let mut lines = input.lines().map(|line| line.split_whitespace());
     let operators = lines.next_back().unwrap().collect::<Vec<_>>();
@@ -12,8 +14,30 @@ fn part1(input: &str) -> usize {
         .sum()
 }
 
-fn part2(_input: &str) -> usize {
-    3263827
+fn part2(input: &str) -> usize {
+    let lines = input.lines().collect::<Vec<_>>();
+    let max_column_index = lines.iter().map(|line| line.len()).max().unwrap();
+
+    let columns = (0..max_column_index)
+        .rev()
+        .map(|column| lines.iter().map(|line| line.chars().nth(column).unwrap_or(' ')).join(""))
+        .collect::<Vec<_>>();
+
+    let mut total = 0;
+    let mut numbers = vec![];
+
+    for column in columns.iter().filter(|c| !c.trim().is_empty()) {
+        let number = column.replace(['+', '*'], "").trim().parse::<usize>().unwrap();
+        numbers.push(number);
+
+        let maybe_operator = column.chars().find(|&c| c == '+' || c == '*');
+        if let Some(operator) = maybe_operator {
+            total += if operator == '+' { numbers.iter().sum::<usize>() } else { numbers.iter().product::<usize>() };
+            numbers.clear();
+        }
+    }
+
+    total
 }
 
 #[cfg(test)]
@@ -35,6 +59,12 @@ mod tests {
     #[test]
     fn solves_part2_example() {
         assert_eq!(part2(EXAMPLE), 3263827)
+    }
+
+    #[test]
+    fn solves_part2() {
+        let input = daily_input(6);
+        assert_eq!(part2(&input), 7903168391557)
     }
 
     const EXAMPLE: &str = "123 328  51 64
